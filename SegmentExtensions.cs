08889a2c4442f9280008782c95dcc6187c8f7abc
DenisLuba <denislubapershins@gmail.com>
2024-01-27 23:53:37 +0300

    Add project files.

---
+++ SegmentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Avalonia.Media;
+using Geometry;
+using Segment = Geometry.Segment;
+
+namespace GeometryPainting;
+
+public static class SegmentExtensions
+{
+    private static readonly Dictionary<Segment, Color> Colors = new();
+
+    public static void SetColor(this Segment segment, Color color) 
+        => Colors[segment] = color;
+
+    public static Color GetColor(this Segment segment) 
+        => Colors.ContainsKey(segment) 
+            ? Colors[segment] 
+            : Avalonia.Media.Colors.Black;
+}
